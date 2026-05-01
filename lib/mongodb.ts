3745808ffbcd6956959ec7d046705411199541af
mbende sampson <mbende2000@yahoo.com>
2026-05-01 16:25:39 +0200

    fix: 10s Atlas connection timeout

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -23,7 +23,11 @@ export async function connectDB() {
   if (cached.conn) return cached.conn
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false })
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 10000,
+      connectTimeoutMS: 10000,
+    })
   }
 
   try {
